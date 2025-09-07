04258cceef8b175364a6db7fce3dcf5c3e4d8a74
Augustin CHINI <a.chini@cilea.fr>
2025-09-07 14:24:54 +0200

    pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Mon budget - Gestionnaire de budget",
+        short_name: "Mon Budget",
+        description: "Application de gestion de budget",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        start_url: ".",
+        scope: ".",
+        icons: [
+          {
+            src: "/avatar.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/avatar.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
